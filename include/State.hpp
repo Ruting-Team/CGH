54833fa2d6e92832316a0aa9c7c665b6f29a90e8
何锦龙 <loringhe@outlook.com>
2018-10-25 12:47:45 +0800

    add annotations for State.hpp

--- include/State.hpp
+++ include/State.hpp
@@ -23,24 +23,44 @@ namespace cgh{
         static ID counter;
 
     protected:
-        ID id;
-        Flag flag;
+        ID id; /// < The unique id for a state.
+        Flag flag; /// < Records some information for this state.
+
+        /// \brief Construction function for this class.
+        ///
+        /// counter is a static variable that it will plus 1 when a new state is created.
+        /// counter maintain that every state has unique id globally.
         State() : id (counter++), flag(0) {}
-        State(ID id) : id(id), flag(0) {}
         virtual ~State() {}
 
     public:
-        void setID(ID i) {id = i;}
+        /// \brief Sets a flag representing this state is final state or not accroding the param b.
+        /// \param b If b is true then set this state to be the final state, otherwise, to be the normal state.
         void setFinalFlag(bool b) {flag = b ? (flag | 1) : (flag & ~1);}
-        void setVisitedFlag(bool b) {flag = b ? (flag | (1 << 1)) : (flag & ~(1 << 1));}
-        void setValidFlag(bool b) {flag = b ? (flag | (1 << 2)) : (flag & ~(1 << 2));}
-        void setEpsilonFlag(bool b) {flag = b ? (flag | (1 << 3)) : (flag & ~(1 << 3));}
 
+        /// \brief Sets a flag representing this state is visited or not accroding the param b.
+        /// \param b If b is true then set this state to be the visited state, otherwise, to be the normal state.
+        void setVisitedFlag(bool b) {flag = b ? (flag | (1 << 1)) : (flag & ~(1 << 1));}
+
+        /// \brief Sets a flag representing this state is valid state or not accroding the param b.
+        /// \param b If b is true then set this state to be the final state, otherwise, to be the unvalid state.
+        void setValidFlag(bool b) {flag = b ? (flag | (1 << 2)) : (flag & ~(1 << 2));}
+
+        /// \brief Gets a ID that is the id of this state.
+        /// \return A ID that is the id of this state.
         ID getID() const {return id;}
+
+        /// \brief Gets a boolean that representing whether this state is a final state. 
+        /// \return A boolean.
         bool isFinal() const {return (flag & 1) == 1;}
+
+        /// \brief Gets a boolean that representing whether this state is visited. 
+        /// \return A boolean.
         bool isVisited() const {return (flag & 1 << 1) == (1 << 1);}
+
+        /// \brief Gets a boolean that representing whether this state is a valid state. 
+        /// \return A boolean.
         bool isValid() const {return (flag & (1 << 2)) == (1 << 2);}
-        bool hasEpsilonTrans() const {return (flag & (1 << 3)) == (1 << 3);}
         
         friend NFA<class Character>;
         friend DFA<class Character>;
