38c88cff669ad146100f91c8ccbaa2384c214dd7
何锦龙 <loringhe@outlook.com>
2018-10-26 08:26:57 +0800

    add a part of annotations for FA.hpp

--- include/FA.hpp
+++ include/FA.hpp
@@ -16,17 +16,15 @@
 
 using namespace std;
 namespace cgh{
-    /*****************  DEC  *****************/
     template <class Character> class DFA;
     template <class Character> class NFA;
     template <class Character> class DFAState;
     template <class Character> class NFAState;
     
-    /*****************  DEF  class FA  *****************/
+    /// \brief FA is a virtual class which define a Finite Automaton.
     template <class Character>
     class FA
     {
-        /*****************  DEF  *****************/
         typedef DFA<Character> DFA;
         typedef NFA<Character> NFA;
         typedef Global<Character> Global;
@@ -48,98 +46,89 @@ namespace cgh{
         typedef typename Global::Char2DFAStateSetMap Char2DFAStateSetMap;
         typedef typename Global::DFAState2NFAStateMap DFAState2NFAStateMap;
         
-        typedef typename Global::FASetIter FASetIter;
-        typedef typename Global::DFASetIter DFASetIter;
-        typedef typename Global::FAListIter FAListIter;
-        typedef typename Global::DFAStateSetIter DFAStateSetIter;
-        typedef typename Global::DFATransMapIter DFATransMapIter;
-        typedef typename Global::NFAStateSetIter NFAStateSetIter;
-        typedef typename Global::NFATransMapIter NFATransMapIter;
-        typedef typename Global::DFAState2MapIter DFAState2MapIter;
-        typedef typename Global::DFAState2MapIter NFAState2MapIter;
-        typedef typename Global::CharacterSetIter CharacterSetIter;
-        typedef typename Global::CharacterSetConstIter CharacterSetConstIter;
-        typedef typename Global::DFAStateSetMapIter DFAStateSetMapIter;
-        typedef typename Global::Char2DFAStateSetMapIter Char2DFAStateSetMapIter;
-        
-        typedef typename Global::FASetConstIter FASetConstIter;
-        typedef typename Global::FAListConstIter FAListConstIter;
-        typedef typename Global::DFAStateSetConstIter DFAStateSetConstIter;
-        typedef typename Global::DFATransMapConstIter DFATransMapConstIter;
-        typedef typename Global::NFAStateSetConstIter NFAStateSetConstIter;
-        typedef typename Global::NFATransMapConstIter NFATransMapConstIter;
-        typedef typename Global::DFAState2MapConstIter DFAState2MapConstIter;
-        typedef typename Global::Char2DFAStateSetMapConstIter Char2DFAStateSetMapConstIter;
-        
     protected:
-        Flag flag;
-        CharacterSet alphabet;
+        Flag flag; /// < Records some attributes for this FA.
+        CharacterSet alphabet;/// < A set of characters which in the label on the transitions.
+
+        /// \brief Default construction without arguments, initialize flag to 0.
         FA():flag(0){}
-        FA(regex regularExpression){}//todo
         FA(FILE *file){}//todo
         virtual ~FA(){}
         void setDeterminateFlag(bool b){flag = b ? (flag | 1):(flag & ~1);}
         void setReachableFlag(bool b){flag = b ? (flag | (1<<1)):(flag & ~(1<<1));}
     private:
-        static void getTransMapByStateSet(const DFAStateSet& stateSet, Char2DFAStateSetMap& nfaTransMap)
-        {
-            DFATransMap& transMap = (*stateSet.begin())->getDFATransMap();
-            DFAStateSet set;
-            for(DFATransMapIter mapIter = transMap.begin(); mapIter != transMap.end(); mapIter++)
-            {
-                set.clear();
-                set.insert(mapIter->second);
-                for(DFAStateSetConstIter iter = ++stateSet.begin(); iter != stateSet.end(); iter++)
-                {
-                    DFAState* state = (*iter)->getTargetStateByChar(mapIter->first);
-                    if(!state) break;
-                    set.insert(state);
+        static void getTransMapByStateSet(const DFAStateSet& stateSet, Char2DFAStateSetMap& nfaTransMap) {
+            DFATransMap& transMap = (*stateSet.begin()) -> getDFATransMap();
+            DFAStateSet workSet;
+            for (auto& mapPair : transMap) {
+                workSet.clear();
+                for (DFAState* dfaState : stateSet) {
+                    DFAState* state = dfaState -> getTargetStateByChar(mapPair.first);
+                    if (!state) break;
+                    workSet.insert(state);
                 }
-                if(set.size() == stateSet.size()) nfaTransMap[mapIter->first] = set;
+                if (workSet.size() == stateSet.size())
+                    nfaTransMap[mapPair.first] = workSet;
             }
         }
         
-        static void makeDFATrans(DFAState* preState, DFAStateSetMap &setMapping, const Char2DFAStateSetMap &nfaTransMap, DFA* dfa)
-        {
+        static void makeDFATrans(DFAState* preState, DFAStateSetMap &setMapping, const Char2DFAStateSetMap &nfaTransMap, DFA* dfa) {
             Char2DFAStateSetMap transMap;
-            for(Char2DFAStateSetMapConstIter mapIter = nfaTransMap.begin(); mapIter != nfaTransMap.end(); mapIter++)
-            {
-                DFAStateSetMapIter setMapIter = setMapping.find(mapIter->second);
+            for (auto& mapPair : nfaTransMap) {
+                auto setMapIter = setMapping.find(mapIter.second);
                 DFAState* postState;
-                if(setMapIter == setMapping.end())
-                {
+                if (setMapIter == setMapping.end()) {
                     transMap.clear();
-                    FA::getTransMapByStateSet(mapIter->second, transMap);
-                    if(DFA::allFinalState(mapIter->second)) postState = dfa->mkDFAFinalState();
-                    else postState = dfa->mkDFAState();
-                    setMapping[mapIter->second] = postState;
+                    FA::getTransMapByStateSet(mapPair.second, transMap);
+                    if(DFA::allFinalState(mapPair.second)) postState = dfa -> mkDFAFinalState();
+                    else postState = dfa -> mkDFAState();
+                    setMapping[mapPair.second] = postState;
                     makeDFATrans(postState, setMapping, transMap, dfa);
+                } else {
+                    postState = (setMapping[mapPair.second]);
                 }
-                else postState = (setMapping[mapIter->second]);
-                preState->addDFATrans(mapIter->first, postState);
+                preState -> addDFATrans(mapPair.first, postState);
             }
         }
+
     public:
-        bool isDeterminate()const{return (flag & 1) == 1;}
-        bool isReachable()const{return (flag & 1<<1) == (1<<1);}
+        /// \brief Judges whether this FA is determinate or not.
+        ///
+        /// ture means determinate, false means nonderminate.
+        /// \return A boolean representing determinacy.
+        bool isDeterminate() const {return (flag & 1) == 1;}
+
+        /// \brief Judges whether this FA is reachable or not.
+        ///
+        /// ture means reachable, false means unreachable.
+        /// reachable means all the states in this FA can be reached from initial state.
+        /// \return A boolean representing reachability.
+        bool isReachable() const {return (flag & 1 << 1) == (1 << 1);}
+
         CharacterSet& getAlphabet() {return alphabet;}
         const CharacterSet& getAlphabet() const {return alphabet;}
         
-        void setAlphabet(const CharacterSet &charSet){alphabet.clear(); alphabet.insert(charSet.begin(),charSet.end());}
-        void setAlphabet(const set<Character> &charSet){alphabet.clear(); alphabet.insert(charSet.begin(),charSet.end());}
+        void setAlphabet(const CharacterSet &charSet) {
+            alphabet.clear();
+            alphabet.insert(charSet.begin(),charSet.end());
+        }
+        void setAlphabet(const set<Character> &charSet) {
+            alphabet.clear();
+            alphabet.insert(charSet.begin(),charSet.end());
+        }
         
         
         virtual bool isNULL() const = 0;
-        virtual FA &operator &(const FA &fa) = 0;//intersection
-        virtual FA &operator |(const FA &fa) = 0;//union
-        virtual DFA &operator !( void ) = 0;//complement
+        virtual FA& operator &(const FA &fa) = 0;//intersection
+        virtual FA& operator |(const FA &fa) = 0;//union
+        virtual DFA& operator !( void ) = 0;//complement
         
-        virtual DFA &determine( void ) = 0;
-        virtual NFA &nondetermine( void ) = 0;
-        virtual FA &concat(const FA &fa) = 0;//concatination
+        virtual DFA& determine( void ) = 0;
+        virtual NFA& nondetermine( void ) = 0;
+        virtual FA& concat(const FA &fa) = 0;//concatination
         
-        virtual FA &rightQuotient(Character character) = 0;
-        virtual FA &leftQuotient(Character character) = 0;
+        virtual FA& rightQuotient(Character character) = 0;
+        virtual FA& leftQuotient(Character character) = 0;
         
         virtual void removeUnreachableState() = 0;
         virtual void removeDeadState() = 0;
@@ -151,7 +140,7 @@ namespace cgh{
         virtual void output()const = 0;
         virtual void print(string filename)const = 0;
         
-        FA &operator -(const FA &fa)//minus
+        FA& operator -(const FA &fa)//minus
         {
             DFA& tempDFA = !const_cast<FA&>(fa);
             FA& res = *this & tempDFA;
@@ -160,24 +149,24 @@ namespace cgh{
         }
         
         
-        static FA &intersectionFA(const FA& lhsfa, const FA& rhsfa)
+        static FA& intersectionFA(const FA& lhsfa, const FA& rhsfa)
         {
             return const_cast<FA&>(lhsfa) & rhsfa;
         }
-        static FA &unionFA(const FA& lhsfa, const FA& rhsfa)
+        static FA& unionFA(const FA& lhsfa, const FA& rhsfa)
         {
             return const_cast<FA&>(lhsfa) | rhsfa;
         }
-        static FA &concat(const FA& lhsfa, const FA& rhsfa)
+        static FA& concat(const FA& lhsfa, const FA& rhsfa)
         {
             return const_cast<FA&>(lhsfa).concat(rhsfa);
         }
-        static FA &minus(const FA& lhsfa, const FA& rhsfa)
+        static FA& minus(const FA& lhsfa, const FA& rhsfa)
         {
             return const_cast<FA&>(lhsfa) - rhsfa;
         }
         
-        static FA &multiIntersection(const FASet &faSet)
+        static FA& multiIntersection(const FASet& faSet)
         {
             DFASet tempDFASet;
             DFAStateSet set;
@@ -211,7 +200,7 @@ namespace cgh{
             return *dfa;
         }
         //        static bool multiIntersectionAndDeterminEmptiness(const FASet &faSet);//todo
-        static FA &multiConcatination(const FAList &faList)
+        static FA& multiConcatination(const FAList& faList)
         {
             NFA *nfa = new NFA();
             NFAState* iniState = nfa->mkNFAInitialState();
@@ -251,7 +240,7 @@ namespace cgh{
             return *nfa;
         }
         
-        static FA &multiUnion(const FASet &faSet)
+        static FA& multiUnion(const FASet& faSet)
         {
             NFA *nfa = new NFA();
             NFAState* iniState = nfa->mkNFAInitialState();
@@ -286,7 +275,7 @@ namespace cgh{
         
         virtual bool isEmpty() = 0;
         
-        bool operator ==(const FA &fa )
+        bool operator ==(const FA& fa )
         {
             DFA& cDFA = !(const_cast<FA&>(fa));
             DFA& iDFA = (*this & cDFA).determine();
@@ -309,7 +298,7 @@ namespace cgh{
             delete &iDFA;
             return true;
         }
-        bool operator <=(const FA &fa )
+        bool operator <=(const FA& fa )
         {
             DFA& cDFA = !(const_cast<FA&>(fa));
             DFA& iDFA = (*this & cDFA).determine();
@@ -323,8 +312,6 @@ namespace cgh{
             delete &iDFA;
             return true;
         }
-        //        void resetVisitedFlag()
-        //        {StateSetIter iter; for(iter=stateSet.begin();iter!=stateSet.end();iter++) (*iter)->setVisitedFlag(0);}
         static DFA& EmptyDFA()
         {
             DFA* dfa = new DFA();
